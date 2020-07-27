e5f3f11a94b935cc75c102d5fcb37729035350dd
xu.gao@zhaopin.com.cn <xu.gao@zhaopin.com.cn>
2020-07-27 21:56:16 +0800

    configstart
    初始化配置中心

---
+++ src/main/java/com/micro/action/maconfigservice/MaConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.action.maconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micro/action/maconfigservice/MaConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.action.maconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
